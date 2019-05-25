bfdaf2b1703fd628f689953a5872f823a4fe6132
hhh <1340545944@qq.com>
2019-05-25 19:02:54 +0800

    improve `Utils` & remove `now`

--- src/common/Utils.ts
+++ src/common/Utils.ts
@@ -1,37 +1,9 @@
 import {
     _PI, _sqrt, _pow, _window, _now, _undefined,
-    _clearTimeout, _setTimeout, _Map, _Object, _null
+    _clearTimeout, _setTimeout, _Map, _Object, _null, _assign
 } from "./references";
 
 export namespace Utils {
-
-    export const Const = {
-        EMPTY_OBJECT: _Object.create(_null) as {},
-        DOUBLE_PI: _PI * 2,
-        HALF_PI: _PI / 2,
-        TRANSPARENT: 'rgba(0,0,0,0)',
-    } as const;
-
-    export const removeIndex = (array: unknown[], index: number) => {
-        const end = array.length - 1;
-        for (; index < end; index++) {
-            array[index] = array[index + 1];
-        }
-        array.length--;
-    };
-
-    export const mix = (a: number, b: number, k: number) => a * k + b * (1 - k);
-
-    export const rad2deg = (rad: number) => rad / _PI * 180;
-
-    export const deg2rad = (deg: number) => deg / 180 * _PI;
-
-    export const quadraticSum = (a: number, b: number) => _pow(a, 2) + _pow(b, 2);
-
-    export const distance = (x1: number, y1: number, x2: number, y2: number) =>
-        _sqrt(quadraticSum(x2 - x1, y2 - y1));
-
-    export const now = _window.performance ? _window.performance.now.bind(_window.performance) : _now;
 
     export type ToArray<T> = T extends any[] ? T : [T];
 
@@ -45,24 +17,6 @@ export namespace Utils {
         delay: number;
     }
 
-    export const debounce = <F extends DebounceCallback>(callback: F, initDelay?: number) => {
-        let timer: any;
-        const wrapper: DebounceWrapper<F> = function debounceWrapper() {
-            if (timer !== _undefined) {
-                _clearTimeout(timer);
-            }
-            type ApplyArgs = [Callback<void>, number, ...any[]];
-            timer = _setTimeout.apply(
-                _window,
-                ([callback, wrapper.delay] as ApplyArgs).concat(arguments) as ApplyArgs
-            );
-        };
-        wrapper.delay = initDelay || debounce.defaultDelay;
-        return wrapper;
-    };
-
-    debounce.defaultDelay = 100;
-
     export type ThrottleCallback = Callback<any>;
 
     export interface ThresholdWrapper<F extends ThrottleCallback> {
@@ -70,29 +24,92 @@ export namespace Utils {
         threshold: number;
     }
 
-    export const throttle = <F extends ThrottleCallback>(callback: F, initThreshold?: number) => {
-        let lastCallTime = 0;
-        const wrapper: ThresholdWrapper<F> = function throttleWrapper(this: any) {
-            const currentTime = _now();
-            if (currentTime - lastCallTime >= wrapper.threshold) {
-                lastCallTime = currentTime;
-                return callback.apply(this, arguments as unknown as unknown[]) as ReturnType<F>;
-            }
-        };
-        wrapper.threshold = initThreshold || throttle.defaultThreshold;
-        return wrapper;
-    };
-
-    throttle.defaultThreshold = 100;
-
     export type CacheWrapper<T extends Callback<any>> =
         Callback<ThisParameterType<T>, Parameters<T>, ReturnType<T>> &
         { cache: Map<string, ReturnType<T>>; };
 
-    export const cache = <T extends Callback<any>>(fn: T): CacheWrapper<T> => {
+    export type ExcludeKeys<O, K extends keyof O> = Pick<O, Exclude<keyof O, K>>;
+
+}
+
+export const Utils = {
+
+    Const: {
+        EMPTY_OBJECT: _Object.create(_null) as {},
+        DOUBLE_PI: _PI * 2,
+        HALF_PI: _PI / 2,
+        TRANSPARENT: 'rgba(0,0,0,0)',
+    } as const,
+
+    removeIndex(array: unknown[], index: number) {
+        const end = array.length - 1;
+        for (; index < end; index++) {
+            array[index] = array[index + 1];
+        }
+        array.length--;
+    },
+
+    mix(a: number, b: number, k: number) {
+        return a * k + b * (1 - k);
+    },
+
+    rad2deg(rad: number) {
+        return rad / _PI * 180;
+    },
+
+    deg2rad(deg: number) {
+        return deg / 180 * _PI;
+    },
+
+    quadraticSum(a: number, b: number) {
+        return _pow(a, 2) + _pow(b, 2);
+    },
+
+    distance(x1: number, y1: number, x2: number, y2: number) {
+        return _sqrt(Utils.quadraticSum(x2 - x1, y2 - y1));
+    },
+
+    debounce: _assign(
+        function debounce<F extends Utils.DebounceCallback>(callback: F, delay?: number) {
+            let timer: any;
+            const wrapper: Utils.DebounceWrapper<F> = function debounceWrapper() {
+                if (timer !== _undefined) {
+                    _clearTimeout(timer);
+                }
+                type ApplyArgs = [Utils.Callback<void>, number, ...any[]];
+                timer = _setTimeout.apply(
+                    _window,
+                    ([callback, wrapper.delay] as ApplyArgs).concat(arguments) as ApplyArgs
+                );
+            };
+            wrapper.delay = delay || Utils.debounce.defaultDelay;
+            return wrapper;
+        }, {
+            defaultDelay: 100
+        }
+    ),
+
+    throttle: _assign(
+        function throttle<F extends Utils.ThrottleCallback>(callback: F, threshold?: number) {
+            let lastCallTime = 0;
+            const wrapper: Utils.ThresholdWrapper<F> = function throttleWrapper(this: any) {
+                const currentTime = _now();
+                if (currentTime - lastCallTime >= wrapper.threshold) {
+                    lastCallTime = currentTime;
+                    return callback.apply(this, arguments as unknown as unknown[]) as ReturnType<F>;
+                }
+            };
+            wrapper.threshold = threshold || Utils.throttle.defaultThreshold;
+            return wrapper;
+        }, {
+            defaultThreshold: 100
+        }
+    ),
+
+    cache<T extends Utils.Callback<any>>(fn: T): Utils.CacheWrapper<T> {
         const map = new _Map<string, ReturnType<T>>();
         const cacheWrapper = function cacheWrapper(
-            this: ThisParameterType<T>, ...args: ToArray<Parameters<T>>
+            this: ThisParameterType<T>, ...args: Utils.ToArray<Parameters<T>>
         ) {
             const parameters = args.join();
             if (map.has(parameters)) {
@@ -105,8 +122,6 @@ export namespace Utils {
         };
         cacheWrapper.cache = map;
         return cacheWrapper;
-    };
+    },
 
-    export type ExcludeKeys<O, K extends keyof O> = Pick<O, Exclude<keyof O, K>>;
-
-}
+} as const;
