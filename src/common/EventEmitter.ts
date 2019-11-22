3c5ec04337e16fc73adb54f478d1e0d62b0fb6a8
hhh <1340545944@qq.com>
2019-11-22 19:19:10 +0800

    improve interface `EventListenerRecord`

--- src/common/EventEmitter.ts
+++ src/common/EventEmitter.ts
@@ -3,7 +3,10 @@ import { Utils } from "./Utils";
 
 export type EventListener<T, A> = Utils.Callback<T, A, void>;
 
-export type EventListenerRecord<T, A> = [EventListener<T, A>, boolean];
+export interface EventListenerRecord<T, A> {
+    listener: EventListener<T, A>;
+    once: boolean;
+}
 
 export class EventEmitter<E extends object = any> {
 
@@ -19,24 +22,23 @@ export class EventEmitter<E extends object = any> {
     private _listenerMap = new _Map<keyof E, EventListenerRecord<this, any>[]>();
 
     on<T extends keyof E>(event: T, listener: EventListener<this, E[T]>, once?: boolean) {
+        once = !!once;
         const { _listenerMap } = this;
         if (_listenerMap.has(event)) {
-            _listenerMap.get(event)!.push([listener, !once]);
+            _listenerMap.get(event)!.push({ listener, once });
         } else {
-            _listenerMap.set(event, [[listener, !once]]);
+            _listenerMap.set(event, [{ listener, once }]);
         }
         return this;
     }
 
     off<T extends keyof E>(event: T, listener: EventListener<this, E[T]>, once?: boolean) {
-        once = !once;
+        once = !!once;
         const { _listenerMap } = this;
         if (_listenerMap.has(event)) {
             const records = _listenerMap.get(event)!,
                 index = records.findIndex(
-                    record =>
-                        record[0] === listener &&
-                        record[1] === once
+                    record => record.listener === listener && record.once === once
                 );
             if (~index) {
                 Utils.removeIndex(records, index);
@@ -55,8 +57,8 @@ export class EventEmitter<E extends object = any> {
             const records = _listenerMap.get(event)!;
             if (records.length) {
                 _listenerMap.set(event, records.filter(record => {
-                    record[0].apply(this, args);
-                    return record[1] && records.includes(record);
+                    record.listener.apply(this, args);
+                    return !record.once && records.includes(record);
                 }));
                 return true;
             }
